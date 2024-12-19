df56cfaa619877a5f452d3702d10f2af9e8a5763
shohei1227 <shohei_n2826@icloud.com>
2024-12-19 15:42:29 +0900

    update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         これは14:01の変更です<br/>これは12:45の変更です<br/>これは12:33の変更です<br/>Hello World!!!<br/>これはテスト作成のReactプロジェクトです。<br/>Edit <code>src/App.js</code> and save to reload.
+         これは12月19日15:42の変更です</br>14:01の変更です<br/>これは12:45の変更です<br/>これは12:33の変更です<br/>Hello World!!!<br/>これはテスト作成のReactプロジェクトです。<br/>Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
